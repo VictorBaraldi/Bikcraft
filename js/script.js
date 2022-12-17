a15689357940823ce7533d11078b456f5a5e96ab
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2022-12-17 11:26:37 -0300

    Estilo com JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-const menu = document.querySelectorAll(".principal a");
+const menu = document.querySelectorAll(".header-menu a");
+
 
 
 function pageSelect(value) {
     const href = value.href;
-    console.log(href);
+    const location = window.location.href;
+    
+    if (location.includes(href)) {
+        value.classList.add('ativo')
+    } else {
+
+    }
 }
 
 menu.forEach(pageSelect)
 
 
 
-console.log(document.location.href);
+
 
 
