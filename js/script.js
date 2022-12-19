679d22150940552774ccd56f73f6244fd3d5112b
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2022-12-18 21:40:32 -0300

    funcionamento perguntas

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 const menu = document.querySelectorAll(".header-menu a");
 
-
-
 function pageSelect(value) {
-    const href = value.href;
-    const location = window.location.href;
-    
-    if (location.includes(href)) {
-        value.classList.add('ativo')
-    } else {
+  const href = value.href;
+  const location = window.location.href;
 
-    }
+  if (location.includes(href)) {
+    value.classList.add("ativo");
+  } else {
+  }
 }
 
-menu.forEach(pageSelect)
+menu.forEach(pageSelect);
 
+//Perguntas
 
+const perguntas = document.querySelectorAll(".perguntas button");
 
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
 
+function ativarPergunta(e) {
+  const target = e.currentTarget;
+  const controls = target.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  const ativo = resposta.classList.contains("ativa");
+  if (ativo) {
+    target.setAttribute("aria-expanded", "true");
+  } else {
+    target.setAttribute("aria-expanded", "false");
+  }
+}
 
-
+perguntas.forEach(eventosPerguntas);
